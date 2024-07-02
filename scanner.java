6fd44f4a28c7dbe50420bfcdc1ede5fee2e65bf3
codehero11 <safrinsara2612@gmail.com>
2024-07-03 02:51:58 +0530

    l

---
+++ scanner.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class scanner {
+    public static void main (String args[])
+    {
+        Scanner sara = new Scanner(System.in);
+        int a= sara.nextInt();
+        int b = sara.nextInt();
+        System.out.print(a+b);
+    }
+
+
+}
+
