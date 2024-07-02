b1abe8a104f7d0b0d873423fbbf67f2d75d7add8
codehero11 <safrinsara2612@gmail.com>
2024-07-02 20:45:31 +0530

    l

--- variables.java
+++ variables.java
@@ -1,9 +1,11 @@
 class variables{
     public static void main(String args[])
     {
-        int box = 67;
-        String box2  ="sara";
-        System.out.print(box + box2);
+        int a = 10;
+        int b = 20;
+        int c = 30;
+        System.out.print(a+b+c);
+        
 
 
     }
