2f7eba924ed363fa050bd0d8c27d0c790bf3741c
codehero11 <safrinsara2612@gmail.com>
2024-07-08 12:23:43 +0530

    l

---
+++ ifelseString.java
@@ -0,0 +1,8 @@
+ class ifelseString {
+    public static void main(String[] args) {
+        String a="one";
+        String b= "one";
+        String c=b;
+        System.out.print(a==c);        
+    }
+}
