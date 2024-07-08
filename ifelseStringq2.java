26a51fb8fbfe2b9c5cfd07da0e1fa55477aa124e
codehero11 <safrinsara2612@gmail.com>
2024-07-08 12:39:38 +0530

    l

---
+++ ifelseStringq2.java
@@ -0,0 +1,10 @@
+ class ifelseStringq2 {
+    public static void main(String[] args) {
+        String a= "one";
+        String b= new String ("one");
+        String c=b;
+        System.out.print(a==c);
+        
+
+    }
+}
---
+++ rcbstring.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class rcbstring {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String RCB = scan.nextLine();
+
+        if (RCB.equals("win")) {
+            System.out.print("Ee sala cup namdhey");
+        } else {
+            System.out.print("No cup");
+        }
+    }
+}
